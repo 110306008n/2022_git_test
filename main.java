7c4663b8a454c1e9517d53a93366c13951b2ad7c
Daniel <s980233@gmail.com>
2022-03-26 21:38:42 +0800

    b2.0

--- main.java
+++ main.java
@@ -1,9 +1,6 @@
 public class main{
     public static void main(String args[]){
-<<<<<<< HEAD
         System.out.println("main_version1.3");
-=======
-        System.out.println("branch_version1.3");
->>>>>>> branch1.0
+        System.out.println("branch_version2.0");
     }
 }
